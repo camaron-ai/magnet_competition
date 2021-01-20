712c554e6b872576fa65384c12659b904178b54d
Yanick Medina <yanickjmedina@gmail.com>
2021-01-20 17:30:44 -0400

    adding xgboost, removing print from predict.py

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import LinearRegression, Lasso
 from lightgbm import LGBMRegressor
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 
 library = {'LR': LinearRegression,
            'LGBM': LGBMRegressor,
            'RF': RandomForestRegressor,
-           'LASSO': Lasso}
+           'LASSO': Lasso,
+           'XGBOOST': XGBRegressor}
