651dda713c2ef9d8d021a2b57802ca7f7eb41d56
Yanick Medina <yanickjmedina@gmail.com>
2021-01-23 21:18:11 -0400

    removing xgboost cause is not installed in docker

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from sklearn.linear_model import LinearRegression, Lasso
 from lightgbm import LGBMRegressor
 from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
+# from xgboost import XGBRegressor
 
 library = {'LR': LinearRegression,
            'LGBM': LGBMRegressor,
            'RF': RandomForestRegressor,
-           'LASSO': Lasso,
-           'XGBOOST': XGBRegressor}
+           'LASSO': Lasso}
+        #    'XGBOOST': XGBRegressor}
+
