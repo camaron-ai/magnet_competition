2e51c4db081a37853d1c8e35b4054961a71722bf
Yanick Medina <yanickjmedina@gmail.com>
2021-02-03 09:51:51 -0400

    modyfing fe

--- preprocessing/fe copy.py
+++ preprocessing/fe copy.py
@@ -15,9 +15,9 @@ def consecutive_count(sequence: np.ndarray):
 
 
 def consecutive_count_above_below_mean(values: np.ndarray):
-    mean = values.mean()
-    count_above_mean = max(consecutive_count(values > mean))
-    count_below_mean = max(consecutive_count(values < mean))
+    mean = np.nanmean(values)
+    count_above_mean = np.nanmax(consecutive_count(values > mean))
+    count_below_mean = np.nanmax(consecutive_count(values < mean))
     return {'count_above_mean': count_above_mean,
             'count_below_mean': count_below_mean}
 
@@ -34,13 +34,30 @@ def cid_ce(values: np.array):
     return np.sqrt(np.square(values - np.roll(values, 1))[1:].mean())
 
 
+def difference_features(values: np.ndarray):
+    output = {}
+    difference = np.square(values - np.roll(values, 1))[1:]
+    output['cid_ce'] = np.sqrt(np.nanmean(difference))
+    # output['d_q0.9'] = np.nanquantile(difference, 0.9)
+    # output['time_since_d_q0.9'] = np.nanargmax((difference >= output['d_q0.9'])[::-1])
+    return output
+
+
 def calculate_features(data: pd.DataFrame,
                        timestep=np.nan) -> Dict[str, float]:
     features = {'timedelta': timestep}
-    for hours in [1*60, 3*60, 7*60]:
+
+    # mean and std
+    for hours in [1*60, 5*60, 10*60, 48*60]:
         last_hours_data = data.iloc[-hours:, :]
         agg_features = last_hours_data.agg(('mean', 'std')).to_dict()
         features[f'{hours//60}h'] = agg_features
+
+    # linear features
+    for hours in [10*60, 48*60]:
+        last_hours_data = data.iloc[-hours:, :]
+        for feature, values in last_hours_data.items():
+            values = values.dropna()
 
     for hours in [48*60]:
         last_2day = data.iloc[-hours:, :]
@@ -50,10 +67,8 @@ def calculate_features(data: pd.DataFrame,
             if len(values) == 0:
                 continue
             _complex_features = calculate_linreg_features(values)
-            _complex_features['cid_ce'] = cid_ce(values)
+            _complex_features.upate(difference_features(values))
             _complex_features.update(consecutive_count_above_below_mean(values))
-            _complex_features['q_0.9'] = np.quantile(values, 0.9)
-            _complex_features['q_0.1'] = np.quantile(values, 0.1)
             complex_features[feature].update(_complex_features)
         features[f'{hours//60}h'] = complex_features
     return join_multiple_dict(features)
